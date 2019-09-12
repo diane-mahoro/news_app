bb48e9bcf7dabefd403e52b9d48b80e4e5f53178
diane-mahoro <www.diammah7@gmail.com>
2019-09-12 17:02:58 +0200

    first function

--- app/request.py
+++ app/request.py
@@ -18,3 +18,21 @@ def get_news(category):
             news_results_list=get_news_response['sources']
             news_results=process_results(news_results_list)
     return news_results
+
+def process_results(news_list):
+    '''
+    function that process the news results
+    '''
+    news_results=[]
+    for news_item in news_list:
+        id=news_item.get('id')
+        name=news_item.get('name')
+        description=news_item.get('description')
+        country=news_item.get('country')
+        poster=news_item.get('url')
+        if poster:
+            news_object=News(id,name,poster,description,country)
+            news_results.append(news_object)
+    return movie_results
+
+
