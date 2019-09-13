0f093d8338b6b5e3ae8f54740aee7645adf6e297
diane-mahoro <www.diammah7@gmail.com>
2019-09-13 16:01:41 +0200

    testing the test case

---
+++ tests/test_review.py
@@ -0,0 +1,17 @@
+import unittest
+from .models import Articles
+Articles=models.Articles
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test class to test  the behaviour of the Articles class
+    '''
+    def setUp(self):
+        self.new_article=Articles("hello","brabra","dfhsjn","fddsec","descdscx","sdfgvh")
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Articles))
+
+if __name__ == '__main__':
+    unittest.main()
+    
